8ef2be461e1b47ac32bf9111978608dee6f01740
Rae7771653 <nicole831220@gmail.com>
2022-12-07 00:03:14 +0800

    feat:add getRestaurants api

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
-const { pages } = require('./routes')
+const { pages, apis } = require('./routes')
 const SESSION_SECRET = 'secret'
 const app = express()
 const port = process.env.PORT || 3000
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
   res.locals.user = getUser(req)
   next()
 })
+app.use('/api', apis)
 app.use(pages)
 
 app.listen(port, () => {
